c4437455d5f1e9d022c574cae8f5fcd556f31eee
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-02-27 20:06:13 +0100

    create table with DDL statement

--- src/main/java/Statement/CreateTableExample.java
+++ src/main/java/Statement/CreateTableExample.java
@@ -1,4 +1,29 @@
 package Statement;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class CreateTableExample {
+    private static final String URL = "jdbc:postgresql://localhost:5432/ksidb";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "admin";
+
+    public static void main(String[] args) {
+        String createTableSQL = """
+                CREATE TABLE IF NOT EXISTS WYDAWCA (
+                    WYDID SERIAL PRIMARY KEY,
+                    NAME VARCHAR(120) NOT NULL
+                );
+                """;
+
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(createTableSQL);
+            System.out.println("Table WYDAWCA has been created!");
+        } catch (SQLException e) {
+            System.out.println("SQL error: " + e.getMessage());
+        }
+    }
 }
