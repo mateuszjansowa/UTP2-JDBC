ac3ec870627bd963272690fc99add04e2dd10c6e
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-02-27 19:56:30 +0100

    show database metadata

--- src/main/java/PostgreSQLConnection.java
+++ src/main/java/PostgreSQLConnection.java
@@ -1,6 +1,5 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Enumeration;
 
 public class PostgreSQLConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/ksidb";
@@ -10,6 +9,16 @@ public class PostgreSQLConnection {
     public static void main(String[] args) {
         try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
             System.out.println("Connected to PostgreSQL!");
+
+            Enumeration<Driver> drivers = DriverManager.getDrivers();
+            while(drivers.hasMoreElements()) {
+                System.out.println("Driver registered: " + drivers.nextElement().getClass().getName()); // org.postgresql.Driver
+            }
+
+            DatabaseMetaData metaData = conn.getMetaData();
+            System.out.println("Database name: " + metaData.getDatabaseProductName()); // PostgreSQL
+            System.out.println("Version: " + metaData.getDatabaseProductVersion()); // 17.3
+            System.out.println("Driver: " + metaData.getDriverName()); // PostgreSQL JDBC Driver
         } catch (SQLException e) {
             System.err.println("Issue connecting: " + e.getMessage());
         }
