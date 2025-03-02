0baed0f6f1c4c3b45a32339b7f1394af2228d672
Mateusz Sowa <mateusz.jan.sowa@gmail.com>
2025-03-02 13:51:22 +0100

    executeUpdate

--- src/main/java/UpdateExample/UpdateExample.java
+++ src/main/java/UpdateExample/UpdateExample.java
@@ -1,30 +1,35 @@
-package SelectExample;
+package UpdateExample;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
 import java.sql.*;
 
-public class SelectExample {
+public class UpdateExample {
     private static final Dotenv dotenv = Dotenv.load();
+
     private static final String URL = "jdbc:postgresql://localhost:5432/ksidb";
     private static final String USER = dotenv.get("DB_USER");
     private static final String PASSWORD = dotenv.get("DB_PASSWORD");
 
     public static void main(String[] args) {
-        String query = "SELECT * FROM AUTOR";
+        String insertSQL = "INSERT INTO AUTOR (NAME) VALUES ('Czeslaw Milosz')";
+        String selectSQL = "SELECT * FROM AUTOR";
 
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
+            int rowsAffected = statement.executeUpdate(insertSQL);
+            System.out.println("Added: " + rowsAffected + " record.");
 
+            ResultSet resultSet = statement.executeQuery(selectSQL);
             while (resultSet.next()) {
                 int id = resultSet.getInt("AUTID");
                 String name = resultSet.getString("Name");
-                System.out.println("ID: " + id + ", Author: " + name);
+                System.out.println("ID: " + id + ", Autor: " + name);
             }
 
         } catch (SQLException e) {
-            System.out.println("SQL Error: " + e.getMessage());
+            System.out.println(e.getMessage());
         }
     }
+
 }
